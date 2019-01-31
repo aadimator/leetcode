ef9e5817b31dc415ce5788ce2499fdbc657a4c30
Aadam <aadimator@gmail.com>
2019-01-31 15:52:14 +0500

    feat:replace brute-force method with better one

--- 1.Two Sums.py
+++ 1.Two Sums.py
@@ -1,14 +1,32 @@
 # https://leetcode.com/problems/two-sum/
 
-class Solution:
+#------------------
+# Brute Force approach
+#------------------
+# class Solution:
+#     def twoSum(self, nums, target):
+#         """
+#         :type nums: List[int]
+#         :type target: int
+#         :rtype: List[int]
+#         """
+#         for i in range(len(nums)):
+#             for j in range(i+1, len(nums)):
+#                 if nums[i] + nums[j] == target:
+#                     return [i, j]
+
+class Solution(object):
     def twoSum(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
         """
+        dict = {}
         for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+            complement = target - nums[i]
+            if complement in dict:
+                return dict[complement], i
+            dict[nums[i]]=i
+        
         
